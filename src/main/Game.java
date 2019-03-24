10c468c75134888db80c36e635fca62c12e76649
RFK <RFK@192.168.16.43>
2019-03-23 21:00:34 -0400

    Added screen class & pixels to "600 Seconds".

--- src/main/Game.java
+++ src/main/Game.java
@@ -1,37 +1,48 @@
-package me.evanschenkman.main;
+package main;
 
 import java.awt.Canvas;
+import java.awt.Color;
 import java.awt.Dimension;
 import java.awt.Graphics;
 import java.awt.Toolkit;
 import java.awt.image.BufferStrategy;
+import java.awt.image.BufferedImage;
+import java.awt.image.DataBufferInt;
 
 import javax.swing.JFrame;
 
+import graphics.Screen;
+
 public class Game extends Canvas implements Runnable {
 	private static final long serialVersionUID = 1L;
-	
+
 	public static int width = Toolkit.getDefaultToolkit().getScreenSize().width;
 	public static int height = Toolkit.getDefaultToolkit().getScreenSize().height;
 	public static int scale = 3;
-	
+
 	private Thread thread;
 	private JFrame frame;
 	private boolean running = false;
-	
+
+	private Screen screen;
+
+	private BufferedImage image = new BufferedImage(width, height, BufferedImage.TYPE_INT_RGB);
+	private int[] pixels = ((DataBufferInt) image.getRaster().getDataBuffer()).getData();
+
 	public Game() {
 		Dimension size = new Dimension(width, height);
 		setPreferredSize(size);
-		
+
 		frame = new JFrame();
+		screen = new Screen(width, height);
 	}
-	
+
 	public synchronized void start() {
 		thread = new Thread(this, "Display");
 		running = true;
 		thread.start();
 	}
-	
+
 	public synchronized void stop() {
 		running = false;
 		try {
@@ -47,9 +58,9 @@ public class Game extends Canvas implements Runnable {
 			render();
 		}
 	}
-	
+
 	private void update() {
-		
+
 	}
 
 	private void render() {
@@ -59,13 +70,21 @@ public class Game extends Canvas implements Runnable {
 			return;
 		}
 		
+		screen.render();
+		
+		for (int i = 0; i < pixels.length; i++) { 
+			pixels[i] = screen.pixels[i];
+		}
+
 		Graphics g = bs.getDrawGraphics();
+		g.setColor(Color.black);
 		g.fillRect(0, 0, width, height);
+		g.drawImage(image, 0, 0, getWidth(), getHeight(), null);
 		g.dispose();
 		bs.show();
 	}
 
-	public static void main(String[] args) { 
+	public static void main(String[] args) {
 		Game game = new Game();
 		game.frame.setResizable(false);
 		game.frame.setTitle("600 Seconds");
@@ -75,7 +94,7 @@ public class Game extends Canvas implements Runnable {
 		game.frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 		game.frame.setLocationRelativeTo(null);
 		game.frame.setVisible(true);
-		
+
 		game.start();
 	}
 }
